fa423b22dddd6422b57be6a1189f3f140edc4ffd
Wellington_Fonseca <thyrso.kof@gmail.com>
2023-02-26 23:09:18 -0300

    feat: Implemente loginController

--- index.js
+++ index.js
@@ -3,16 +3,13 @@ const bodyParser = require('body-parser');
 const app = express();
 app.use(bodyParser.json());
 
-const loginModel = require('./src/models/loginModel');
+const loginController = require('./src/controllers/loginController');
 
 
 const PORT = process.env.PORT || 3000;
 
-app.get('/login', async (req, res) => {
-  const { name } = req.body;
-  const user = await loginModel.findUser(name);
-  res.status(200).json(user)
-})
+app.get('/login', loginController.findUser);  
+
 
 app.listen(PORT, () => {
   console.log(`Listening on port ${PORT}` );
